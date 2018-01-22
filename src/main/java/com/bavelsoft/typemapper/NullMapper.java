7b9e80e5817788710084d084ced6c59e710f7d7c
spoerri <spoerri@users.noreply.github.com>
2018-01-22 14:39:23 -0500

    Update NullMapper.java

--- src/main/java/com/bavelsoft/typemapper/NullMapper.java
+++ src/main/java/com/bavelsoft/typemapper/NullMapper.java
@@ -1,5 +1,13 @@
 package com.bavelsoft.typemapper;
   
-//TODO
-public class NullMapper {
+//TODO rename
+public class NullMapper {  
+	default double map(Double x) { return x == null ? 0 : x; }
+	default float map(Float x) { return x == null ? 0 : x; }
+	default long map(Long x) { return x == null ? 0 : x; }
+	default int map(Integer x) { return x == null ? 0 : x; }
+	default short map(Short x) { return x == null ? 0 : x; }
+	default byte map(Byte x) { return x == null ? 0 : x; }
+	default char map(Character x) { return x == null ? 0 : x; }
+	default boolean map(Boolean x) { return x == null ? false : x; }
 }
