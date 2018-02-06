5c826520d9e2d6acb3ae912ca864c51630ac3f48
Joshua Spoerri <josh@Joshuas-MacBook-Pro.local>
2018-02-05 22:28:24 -0500

    extract class

---
+++ src/main/java/com/bavelsoft/typemapper/impl/Generator.java
@@ -0,0 +1,61 @@
+package com.bavelsoft.typemapper.impl;
+
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+import java.util.Map;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.ExecutableType;
+import com.bavelsoft.typemapper.FieldMatcher;
+import com.bavelsoft.typemapper.FieldMatcher.StringPair;
+import com.bavelsoft.typemapper.TypeMap;
+
+class Generator {
+	static  TypeSpec.Builder generateMapperClass(Element element, Elements elementUtils, Types typeUtils) {
+		TypeSpec.Builder type = TypeSpec.classBuilder(getClassName(element));
+		if (element.getKind() == ElementKind.INTERFACE)
+			type.addSuperinterface(TypeName.get(element.asType()));
+		else
+			type.superclass(TypeName.get(element.asType()));
+		
+		boolean hasUnimplemented = false;
+		for (Element e : elementUtils.getAllMembers((TypeElement)element)) {
+			if (e.getKind() == ElementKind.METHOD && e.getAnnotation(FieldMatchSupport.typeMapClass) != null) {
+				type.addMethod(generateMapperMethod((ExecutableElement)e, elementUtils, typeUtils).build());
+			} else if (e.getKind() == ElementKind.METHOD && Util.isAbstract(e)) {
+				hasUnimplemented = true;
+			}
+		}
+		if (hasUnimplemented)
+			type.addModifiers(Modifier.ABSTRACT);
+		return type;
+	}
+
+	static MethodSpec.Builder generateMapperMethod(ExecutableElement methodElement, Elements elementUtils, Types typeUtils) {
+		TypeMap annotation = methodElement.getAnnotation(FieldMatchSupport.typeMapClass);
+		MethodTemplate template = new MethodTemplate(methodElement, elementUtils, typeUtils);
+
+		MethodSpec.Builder method = MethodSpec.overriding(methodElement)
+			.addStatement(template.replace(annotation.first()));
+
+		Map<String, StringPair> map = FieldMatchSupport.getMatchedFields(methodElement, template);
+		for (Map.Entry<String, StringPair> entry : map.entrySet()) {
+			template.setPerFieldValues(entry);
+			method.addStatement(template.replace(annotation.perField()));
+		}
+		return method.addStatement(template.replace(annotation.last()));
+	}
+
+	static String getClassName(Element element) {
+		String name = element.getSimpleName().toString() + "TypeMapper";
+		if (element.getEnclosingElement().getKind() == ElementKind.CLASS)
+			name = element.getEnclosingElement().getSimpleName().toString() + "_" + name;
+		return name;
+	}
+}
--- src/main/java/com/bavelsoft/typemapper/impl/TypeMapProcessor.java
+++ src/main/java/com/bavelsoft/typemapper/impl/TypeMapProcessor.java
@@ -1,23 +1,10 @@
 package com.bavelsoft.typemapper.impl;
 
-import com.squareup.javapoet.JavaFile;
-import com.squareup.javapoet.TypeName;
-import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.TypeSpec;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Set;
-import java.util.List;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.HashSet;
-import java.util.HashMap;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.ArrayList;
-import java.util.function.BiFunction;
-import java.util.function.Supplier;
 import javax.tools.FileObject;
 import javax.tools.JavaFileObject;
 import javax.annotation.processing.Filer;
@@ -30,24 +17,15 @@ import javax.tools.Diagnostic;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
-import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.ExecutableType;
-import javax.lang.model.type.MirroredTypeException;
-import javax.lang.model.type.TypeMirror;
-import com.google.auto.service.AutoService;
-import com.bavelsoft.typemapper.TypeMap;
 import com.bavelsoft.typemapper.ExpectedException;
-import com.bavelsoft.typemapper.Field;
-import com.bavelsoft.typemapper.Fields;
-import com.bavelsoft.typemapper.FieldMatcher;
-import com.bavelsoft.typemapper.FieldMatcher.StringPair;
+import com.google.auto.service.AutoService;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.TypeSpec;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import static java.util.Arrays.asList;
 
@@ -74,7 +52,7 @@ public class TypeMapProcessor extends AbstractProcessor {
 			elements.add(element.getEnclosingElement());
 		for (Element element : elements) {
 			try {
-				write(element, generateMapperClass(element).build());
+				write(element, Generator.generateMapperClass(element, elementUtils, typeUtils).build());
 			} catch (ExpectedException e) {
 				messager.printMessage(Diagnostic.Kind.ERROR,
 						      "couldn't generate field mapper for "+element+" : "+ e.getMessage());
@@ -86,55 +64,13 @@ public class TypeMapProcessor extends AbstractProcessor {
 		return true;
 	}
 
-	private TypeSpec.Builder generateMapperClass(Element element) {
-		TypeSpec.Builder type = TypeSpec.classBuilder(getClassName(element));
-		if (element.getKind() == ElementKind.INTERFACE)
-			type.addSuperinterface(TypeName.get(element.asType()));
-		else
-			type.superclass(TypeName.get(element.asType()));
-		
-		boolean hasUnimplemented = false;
-		for (Element e : elementUtils.getAllMembers((TypeElement)element)) {
-			if (e.getKind() == ElementKind.METHOD && e.getAnnotation(FieldMatchSupport.typeMapClass) != null) {
-				type.addMethod(generateMapperMethod((ExecutableElement)e).build());
-			} else if (e.getKind() == ElementKind.METHOD && Util.isAbstract(e)) {
-				hasUnimplemented = true;
-			}
-		}
-		if (hasUnimplemented)
-			type.addModifiers(Modifier.ABSTRACT);
-		return type;
-	}
-
-	private MethodSpec.Builder generateMapperMethod(ExecutableElement methodElement) {
-		TypeMap annotation = methodElement.getAnnotation(FieldMatchSupport.typeMapClass);
-		MethodTemplate template = new MethodTemplate(methodElement, elementUtils, typeUtils);
-
-		MethodSpec.Builder method = MethodSpec.overriding(methodElement)
-			.addStatement(template.replace(annotation.first()));
-
-		Map<String, StringPair> map = FieldMatchSupport.getMatchedFields(methodElement, template);
-		for (Map.Entry<String, StringPair> entry : map.entrySet()) {
-			template.setPerFieldValues(entry);
-			method.addStatement(template.replace(annotation.perField()));
-		}
-		return method.addStatement(template.replace(annotation.last()));
-	}
-
 	private void write(Element element, TypeSpec typeSpec) throws IOException {
 		String packageName = elementUtils.getPackageOf(element).toString();
-		JavaFileObject javaFileObject = filer.createSourceFile(packageName+"."+getClassName(element));
+		JavaFileObject javaFileObject = filer.createSourceFile(packageName+"."+Generator.getClassName(element));
 		Writer writer = javaFileObject.openWriter();
 		JavaFile javaFile = JavaFile.builder(packageName, typeSpec).build();
 		javaFile.writeTo(writer);
 		writer.close();
-	}
-
-	private String getClassName(Element element) {
-		String name = element.getSimpleName().toString() + "TypeMapper";
-		if (element.getEnclosingElement().getKind() == ElementKind.CLASS)
-			name = element.getEnclosingElement().getSimpleName().toString() + "_" + name;
-		return name;
 	}
 
 	@Override
